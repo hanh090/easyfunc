952ba2e8c4f437c377d3fc68809ad6c2dfaddc9b
hanh090 <hanh.le@big-labs.com>
2013-09-12 15:22:14 +0700

    Hanh Le - Initiate view module

---
+++ easyfunc-view/easyfunc-view.test/src/main/java/org/easyfunc/view/core/ActivatorTest.java
@@ -0,0 +1,16 @@
+package org.easyfunc.view.core;
+
+
+
+/**
+* Sample integration test. In Eclipse, right-click > Run As > JUnit-Plugin. <br/>
+* In Maven CLI, run "mvn integration-test".
+*/
+public class ActivatorTest {
+
+//	@Test
+//	public void veryStupidTest() {
+//		assertEquals("easyfunc-view.core",Activator.PLUGIN_ID);
+//		assertTrue("Plugin should be started", Activator.getDefault().started);
+//	}
+}
