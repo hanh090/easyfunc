31943b3bc7cdb896408ea905f9fea90b9d672d3b
Hanh Le <lehanh.bk.mt1002@gmail.com>
2013-09-18 03:28:37 +0700

    Hanh Le - Init some more class for module

---
+++ easyfunc-code-generation/src/main/java/code/generation/CodeGeneration.java
@@ -0,0 +1,5 @@
+package code.generation;
+
+public class CodeGeneration {
+
+}
---
+++ easyfunc-code-generation/src/main/java/main/CodeGenerationMain.java
@@ -0,0 +1,9 @@
+package main;
+
+import searcher.Searcher;
+
+public class CodeGenerationMain {
+	public static void main(String[] args) {
+		new Searcher().doSearch();
+	}
+}
