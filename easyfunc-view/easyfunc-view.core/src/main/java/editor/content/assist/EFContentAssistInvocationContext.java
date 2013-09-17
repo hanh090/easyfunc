b4627114d02f3524003f762ffe8b2adc029c5766
Hanh Le <lehanh.bk.mt1002@gmail.com>
2013-09-18 03:31:43 +0700

    Hanh Le - Re-create project independent on tycho

---
+++ easyfunc-view/easyfunc-view.core/src/main/java/editor/content/assist/EFContentAssistInvocationContext.java
@@ -0,0 +1,59 @@
+package editor.content.assist;
+
+import org.eclipse.jdt.internal.ui.JavaPlugin;
+import org.eclipse.jdt.ui.text.java.ContentAssistInvocationContext;
+import org.eclipse.jdt.ui.text.java.JavaContentAssistInvocationContext;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+
+public class EFContentAssistInvocationContext extends
+		JavaContentAssistInvocationContext {
+	public static final char EF_TRIGGER = '\'';
+
+	private CharSequence fPrefix;
+	private boolean ignoreSpace = false;
+
+	public EFContentAssistInvocationContext(
+			ContentAssistInvocationContext context) {
+		// TODO Auto-generated constructor stub
+		super(context.getViewer(), context.getInvocationOffset(), JavaPlugin
+				.getActivePage().getActiveEditor());
+	}
+
+	@Override
+	public CharSequence computeIdentifierPrefix() throws BadLocationException {
+		// TODO Auto-generated method stub
+		if (fPrefix == null) {
+			IDocument document = getDocument();
+			if (document == null)
+				return null;
+			int end = getInvocationOffset();
+			int start = end;
+			if (document.getChar(--start) == EF_TRIGGER)
+				ignoreSpace = true;
+			while (--start >= 0) {
+				if (!Character.isJavaIdentifierPart(document.getChar(start))) {
+
+					if (ignoreSpace) {
+						if (Character.isWhitespace(document.getChar(start))
+								|| document.getChar(start) == '<'
+								|| document.getChar(start) == '>') {
+							continue;
+						}
+						end--;
+					}
+					break;
+				}
+
+			}
+			start++;
+			fPrefix = document.get(start, end - start);
+		}
+
+		return fPrefix;
+	}
+
+	public boolean getIgnoreSpace() {
+		return ignoreSpace;
+	}
+}
---
+++ easyfunc-view/easyfunc-view.core/src/main/java/main/Activator.java
@@ -0,0 +1,53 @@
+package main;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "com.hcmut.easyfunc"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		System.out.println("Started");
+		System.out.println("Activator.start()");
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		System.out.println("Activator.stop()");
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
