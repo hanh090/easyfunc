1d4efb3f2bb009a331518637616df0e2a465b990
hanh090 <hanh.le@big-labs.com>
2013-09-18 18:01:54 +0700

    Hanh Le - Add Java Document

--- easyfunc-indexing/src/main/java/document/MethodDocument.java
+++ easyfunc-indexing/src/main/java/document/MethodDocument.java
@@ -1,16 +1,42 @@
 package document;
 
+import java.util.List;
+
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Store;
 import org.jsoup.select.Elements;
 
-public class MethodDocument extends ApiDocumentFactory{
+import config.ApiField;
 
-	public MethodDocument(Elements elements) {
-		super(elements);
+public class MethodDocument extends ApiDocument{
+
+	@Override
+	public List<Field> index() {
+		
+		return null;
+	}
+
+
+	@Override
+	public boolean update(String id, String value) {
+		
+		return false;
 	}
 
 	@Override
-	public ApiDocument createDocument() {
-		return null;
+	public boolean delete(String id, String value) {
+		
+		return false;
 	}
+
+	@Override
+	public boolean add(String id, String value, Store store) {
+		this.document.add(new ApiField(id, value, store));
+		return false;
+	}
+
+	
+
+	
 	
 }
--- easyfunc-indexing/src/main/java/document/TypeDocument.java
+++ easyfunc-indexing/src/main/java/document/TypeDocument.java
@@ -1,16 +1,40 @@
 package document;
 
+import java.util.List;
+
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.Field.Store;
 import org.jsoup.select.Elements;
 
-public class TypeDocument extends ApiDocumentFactory {
+import config.ApiField;
 
-	public TypeDocument(Elements elements) {
-		super(elements);
-	}
+public class TypeDocument extends ApiDocument {
 
 	@Override
-	public ApiDocument createDocument() {
+	public List<Field> index() {
+		
 		return null;
 	}
 
+	@Override
+	public boolean add(String id, String value, Store store) {
+		this.document.add(new ApiField(id, value, store));
+		return false;
+	}
+
+	@Override
+	public boolean update(String id, String value) {
+		
+		return false;
+	}
+
+	@Override
+	public boolean delete(String id, String value) {
+		
+		return false;
+	}
+
+
+	
 }
