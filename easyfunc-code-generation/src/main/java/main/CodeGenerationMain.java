7e2f964613ec97504db8dc39189c73390044d6af
Hanh Le <lehanh.bk.mt1002@gmail.com>
2013-09-27 16:55:47 +0700

    Hanh Le - Modifield to control the flow of programe

--- easyfunc-code-generation/src/main/java/main/CodeGenerationMain.java
+++ easyfunc-code-generation/src/main/java/main/CodeGenerationMain.java
@@ -1,9 +1,27 @@
 package main;
 
+import code.generation.CodeGeneration;
 import searcher.Searcher;
+import searcher.Searcher.DocumentKind;
 
 public class CodeGenerationMain {
+	private Searcher searcher;
+	private CodeGeneration codeGeneration;
+	
 	public static void main(String[] args) {
-		new Searcher().doSearch();
+		CodeGenerationMain codeGenerationMain = new CodeGenerationMain();
+		codeGenerationMain.generate(args[0]);
+	}
+	
+	
+	/**
+	 * Generate code from {@code fromQuery}.
+	 * @param fromQuery user query which inputed in view module
+	 * @return
+	 */
+	public String generate(String fromQuery) {
+		searcher = new Searcher(fromQuery, DocumentKind.METHOD);
+		searcher.doSearch();
+		return null;
 	}
 }
